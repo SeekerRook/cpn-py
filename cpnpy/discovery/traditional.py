d8758ca0a646278c419d91db342b68e6a9e1c8df
Alessandro Berti <javert@northwood.northwood.net>
2024-12-25 18:07:03 +0100

    added configuration options

--- cpnpy/discovery/traditional.py
+++ cpnpy/discovery/traditional.py
@@ -7,9 +7,27 @@ from cpnpy.util import rv_to_stri
 from cpnpy.cpn.cpn_imp import *
 
 
+def last_non_null(series):
+    """
+    Returns the last non-null value of a pandas Series.
+    If all are null, returns None (or np.nan, depending on preference).
+    """
+    non_null = series.dropna()
+    if not non_null.empty:
+        return non_null.iloc[-1]
+    else:
+        return None  # or np.nan
+
+
 def apply(log: EventLog, pro_disc_alg=pm4py.discover_petri_net_inductive, parameters: Optional[Dict[str, Any]] = None):
     if parameters is None:
         parameters = {}
+
+    original_log_cases_in_im = parameters.get("original_log_cases_in_im", False)
+    original_case_attributes = parameters.get("original_case_attributes", {"case:concept:name"})
+    num_simulated_cases = parameters.get("num_simulated_cases", 1)
+
+    log = pm4py.convert_to_dataframe(log)
 
     net, im, fm = pro_disc_alg(log, parameters)
     stochastic_map = replay.get_map_from_log_and_net(log, net, im, fm)
@@ -28,7 +46,7 @@ def apply(log: EventLog, pro_disc_alg=pm4py.discover_petri_net_inductive, parame
         cpn.add_place(p)
 
     for trans in net.transitions:
-        t = Transition(trans.label if trans.label is not None else "SILENT@"+str(trans.name), variables=["C"])
+        t = Transition(trans.label if trans.label is not None else "SILENT@" + str(trans.name), variables=["C"])
         dict_transitions[trans.name] = t
         cpn.add_transition(t)
 
@@ -38,11 +56,23 @@ def apply(log: EventLog, pro_disc_alg=pm4py.discover_petri_net_inductive, parame
         else:
             trans = arc.source
 
-            cpn.add_arc(Arc(dict_transitions[trans.name], dict_places[arc.target.name], "C"+stochastic_map[trans]))
+            cpn.add_arc(Arc(dict_transitions[trans.name], dict_places[arc.target.name], "C" + stochastic_map[trans]))
 
     marking = Marking()
     for p in im:
-        marking.set_tokens(dict_places[p.name].name, [frozendict({})])
+        if original_log_cases_in_im:
+            result_dict = log.groupby("case:concept:name").agg(last_non_null).to_dict(orient='index')
+
+            lst = []
+            for c, vv in result_dict.items():
+                lst.append(frozendict({k: v for k, v in vv.items() if k in original_case_attributes}))
+            marking.set_tokens(dict_places[p.name].name,
+                               lst)
+        else:
+
+            marking.set_tokens(dict_places[p.name].name,
+                               [frozendict({"case:concept:name": "CASE_" + str(i + 1)}) for i in
+                                range(num_simulated_cases)])
 
     code = """
 from scipy.stats import norm, uniform, expon, lognorm, gamma
@@ -50,4 +80,3 @@ from scipy.stats import norm, uniform, expon, lognorm, gamma
     context = EvaluationContext(user_code=code)
 
     return cpn, marking, context
-
