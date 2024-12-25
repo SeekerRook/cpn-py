62261955479c633a9e61e65667eff2b99c2f0ba4
Alessandro Berti <javert@northwood.northwood.net>
2024-12-25 18:37:46 +0100

    traditional log discovery

--- cpnpy/discovery/traditional.py
+++ cpnpy/discovery/traditional.py
@@ -3,6 +3,8 @@ from frozendict import frozendict
 from pm4py.objects.log.obj import EventLog
 from pm4py.objects.petri_net.obj import PetriNet
 from pm4py.algo.simulation.montecarlo.utils import replay
+from pm4py.algo.decision_mining import algorithm as decision_mining
+from cpnpy.util import simp_guard
 from cpnpy.util import rv_to_stri
 from cpnpy.cpn.cpn_imp import *
 
@@ -25,13 +27,18 @@ def apply(log: EventLog, parameters: Optional[Dict[str, Any]] = None):
 
     num_simulated_cases = parameters.get("num_simulated_cases", 1)
     pro_disc_alg = parameters.get("pro_disc_alg", pm4py.discover_petri_net_inductive)
-    original_log_cases_in_im = parameters.get("original_log_cases_in_im", False)
     original_case_attributes = parameters.get("original_case_attributes", {"case:concept:name"})
+    enable_guards_discovery = parameters.get("enable_guards_discovery", False)
 
     log = pm4py.convert_to_dataframe(log)
 
     # applies a process discovery algorithm in pm4py, discovering an accepting Petri net from a traditional event log
     net, im, fm = pro_disc_alg(log, parameters)
+    if enable_guards_discovery:
+        # if the discovery of the guards is enabled, discover the guards on the transitions
+        # (conditions regulating the execution).
+        net, im, fm = decision_mining.create_data_petri_nets_with_decisions(log, net, im, fm)
+
     # discovers a stochastic map, associating each transition of the original accepting Petri net with a stochastic
     # variable indicating the delay
     stochastic_map = replay.get_map_from_log_and_net(log, net, im, fm)
@@ -51,8 +58,26 @@ def apply(log: EventLog, parameters: Optional[Dict[str, Any]] = None):
         dict_places[place.name] = p
         cpn.add_place(p)
 
+    trans_guards = {}
     for trans in net.transitions:
-        t = Transition(trans.label if trans.label is not None else "SILENT@" + str(trans.name), variables=["C"])
+        if "guard" in trans.properties:
+            eval = simp_guard.parse_boolean_expression(trans.properties["guard"],
+                                                       variables_of_interest=list(original_case_attributes))
+            if str(eval) != "True":
+                trans_guards[trans.name] = str(eval)
+
+    original_log_cases_in_im = parameters.get("original_log_cases_in_im", len(trans_guards) > 0)
+
+    for trans in net.transitions:
+        guard = None
+        if trans.name in trans_guards:
+            guard = trans_guards[trans.name]
+            for att in original_case_attributes:
+                guard = guard.replace(att, "C[\"" + att + "\"]")
+                pass
+
+        t = Transition(trans.label if trans.label is not None else "SILENT@" + str(trans.name), variables=["C"],
+                       guard=guard)
         dict_transitions[trans.name] = t
         cpn.add_transition(t)
 
@@ -61,8 +86,11 @@ def apply(log: EventLog, parameters: Optional[Dict[str, Any]] = None):
             cpn.add_arc(Arc(dict_places[arc.source.name], dict_transitions[arc.target.name], "C"))
         else:
             trans = arc.source
-
-            cpn.add_arc(Arc(dict_transitions[trans.name], dict_places[arc.target.name], "C" + stochastic_map[trans]))
+            if trans in stochastic_map:
+                cpn.add_arc(
+                    Arc(dict_transitions[trans.name], dict_places[arc.target.name], "C" + stochastic_map[trans]))
+            else:
+                cpn.add_arc(Arc(dict_transitions[trans.name], dict_places[arc.target.name], "C"))
 
     marking = Marking()
     for p in im:
